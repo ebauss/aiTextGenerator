efed021d9d152e08a6d61f02659b44849a4bfbca
Evon Bausa <evon.bausa@gmail.com>
2022-11-18 21:14:45 -0800

    push this

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from happytransformer import HappyGeneration
-from happytransformer import GENSettings
+# from happytransformer import HappyGeneration
+# from happytransformer import GENSettings
+#
+# happy_gen = HappyGeneration("GPT-NEO", "EleutherAI/gpt-neo-2.7B")
+#
+# args = GENSettings(min_length=50, max_length=50, do_sample=True, temperature=0.9)
+#
+# result = happy_gen.generate_text("how to get rich", args=args)
+#
+# print(result.text)
 
-happy_gen = HappyGeneration("GPT-NEO", "EleutherAI/gpt-neo-2.7B")
-
-args = GENSettings(min_length=500, max_length=500, no_repeat_ngram_size=2,
-                   do_sample=True, early_stopping=False, top_k=50, temperature=0.7)
-
-result = happy_gen.generate_text("How to learn how to code", args=args)
-
-print(result.text)
+from transformers import pipeline
+generator = pipeline('text-generation', model ='EleutherAI/gpt-neo-2.7B')
+post = "This is my first blog post, I'm really excited!"
+result = generator(post, max_length=50, do_sample=True, temperature=0.9)
+print(result)
